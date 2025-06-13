a7308875dd142090f88019b030c26d60a4cd6899
shafi <shariyarshafi27@gmail.com>
2025-06-14 01:49:35 +0600

    gets data from backend

--- receipe/vege/views.py
+++ receipe/vege/views.py
@@ -1,16 +1,20 @@
+
 from django.shortcuts import render
 from .models import *
+
 def receipes(request):
-    if request.method=="POST":
-        data=request.POST
-        receipe_name=data.get('receipe_name')
-        receipe_desc=data.get('receipe_desc')
-        receipe_image=request.FILES.get('receipe_image')
+    if request.method == "POST":
+        data = request.POST
+        receipe_name = data.get('receipe_name')
+        receipe_desc = data.get('receipe_desc')
+        receipe_image = request.FILES.get('receipe_image')
+
         Receipe.objects.create(
             receipe_name=receipe_name,
-             receipe_desc= receipe_desc,
-              receipe_image= receipe_image
-         )
-    return render(request,'receipes.html')
-# Create your views here.
-       
+            receipe_desc=receipe_desc,
+            receipe_image=receipe_image
+        )
+
+    queryset = Receipe.objects.all()
+    context = {'recipes': queryset}
+    return render(request, 'receipes.html', context)
