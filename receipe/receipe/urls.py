ef207cdf25b3e09b725d40d664f6f3da6d005919
shafi <shariyarshafi27@gmail.com>
2025-06-13 22:22:25 +0600

    templates of base,recipes and urls are created

--- receipe/receipe/urls.py
+++ receipe/receipe/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import *
+from vege.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home,name="home"),
+    path('receipes/',receipes,name="receipes"),
 ]
